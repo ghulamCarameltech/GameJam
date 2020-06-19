9a6d47137f079eb4ac323418bd928584764af11f
Muhammad Ibrahim <muhammad.ibrahim@carameltec.com>
2020-06-19 16:07:47 +0500

    First Commit for part 1

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public delegate void OnTapping();
+    public static event OnTapping onTap; 
+
+    public delegate void OnBack();
+    public static event OnBack onBack; 
+
+    public static bool Enable { get; set; }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(onBack != null)
+            {
+                onBack();
+            }
+        }
+
+        if (!Enable) return;
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began && onTap != null)
+            {
+                onTap();
+            }
+        }
+
+        #if UNITY_EDITOR
+            if(Input.GetKeyDown(KeyCode.Space) && onTap != null)
+            {
+                onTap();
+            }
+        #endif
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 1f;
+
+    public float Speed
+    {
+        get { return _speed; }
+    }
+
+    public static bool PlayerEnable { get; set; }
+
+    Rigidbody _rb;
+
+    void OnEnable()
+    {
+        InputController.onTap += ThrowBall;
+    }
+    
+    void OnDisable()
+    {
+        InputController.onTap -= ThrowBall;
+    }
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+        InputController.Enable = true;
+    }
+
+    public void Initialize ()
+    {
+        // gameObject.transform.position = _path[0].position;
+    }
+
+    void ThrowBall()
+    {
+       _rb.isKinematic = false;
+
+       // _rb.AddForce(new Vector3(0,100,1400)); // Missed Shot
+
+    //    _rb.AddForce(new Vector3(0,100,1200)); //Nice Shoot
+
+        // _rb.AddForce(new Vector3(0,100,1300)); //Good Shoot
+
+        _rb.AddForce(new Vector3(0,100,1378)); // Perfect shoot
+    }
+    
+}
